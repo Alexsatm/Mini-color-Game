6facb4edf20b00c756420f0d52594031d41d5260
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-15 23:03:30 +0300

    feat: variable js

---
+++ app.js
@@ -0,0 +1,3 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 600;
+const colors = ['#BC5D58', '#18FF5D', '#C1876B', '#29AB87', '#D0F0C0', '#4B0082', '#FF8243', '#FF8243', '#F3DA0B', '#20B2AA']
