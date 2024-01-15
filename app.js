f0385ce44041fe9956b930736a261b6f462779b7
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-15 23:03:55 +0300

    feat: add for js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const board = document.querySelector('#board');
 const SQUARES_NUMBER = 600;
 const colors = ['#BC5D58', '#18FF5D', '#C1876B', '#29AB87', '#D0F0C0', '#4B0082', '#FF8243', '#FF8243', '#F3DA0B', '#20B2AA']
+
+for(let i = 0; i < SQUARES_NUMBER; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
